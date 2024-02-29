8b33364fa9519a9b8d30bf93aac3f29fc94c036e
harrisishaq <mynameharris7@gmail.com>
2024-02-29 23:08:46 +0800

    Add Get By Email Repo

--- repository/user_repository.go
+++ repository/user_repository.go
@@ -5,9 +5,10 @@ import "intikom_test/entity"
 type (
 	UserRepository interface {
 		Create(model entity.User) (uint, error)
+		Delete(model *entity.User) error
 		Get(id string) (*entity.User, error)
+		GetByEmail(email string) (*entity.User, error)
 		List(limit, offset int) ([]entity.User, int64, error)
 		Update(model *entity.User) error
-		Delete(model *entity.User) error
 	}
 )
--- repository/user_repository_impl.go
+++ repository/user_repository_impl.go
@@ -33,9 +33,30 @@ func (repo *userRepository) Create(model entity.User) (uint, error) {
 	return model.ID, nil
 }
 
+func (repo *userRepository) Delete(model *entity.User) error {
+	qResult := repo.DB.Clauses(clause.Returning{}).Delete(model)
+	if errors.Is(qResult.Error, gorm.ErrRecordNotFound) {
+		return nil
+	} else if qResult.Error != nil {
+		return qResult.Error
+	}
+	return nil
+}
+
 func (repo *userRepository) Get(id string) (*entity.User, error) {
 	var result entity.User
 	qResult := repo.DB.First(&result, "id = ?", id)
+	if errors.Is(qResult.Error, gorm.ErrRecordNotFound) {
+		return nil, nil
+	} else if qResult.Error != nil {
+		return nil, qResult.Error
+	}
+	return &result, nil
+}
+
+func (repo *userRepository) GetByEmail(email string) (*entity.User, error) {
+	var result entity.User
+	qResult := repo.DB.First(&result, "email = ?", email)
 	if errors.Is(qResult.Error, gorm.ErrRecordNotFound) {
 		return nil, nil
 	} else if qResult.Error != nil {
@@ -70,16 +91,6 @@ func (repo *userRepository) List(limit, offset int) ([]entity.User, int64, error
 
 func (repo *userRepository) Update(model *entity.User) error {
 	qResult := repo.DB.Select("*").Where("id = ?", model.ID).Updates(model)
-	if errors.Is(qResult.Error, gorm.ErrRecordNotFound) {
-		return nil
-	} else if qResult.Error != nil {
-		return qResult.Error
-	}
-	return nil
-}
-
-func (repo *userRepository) Delete(model *entity.User) error {
-	qResult := repo.DB.Clauses(clause.Returning{}).Delete(model)
 	if errors.Is(qResult.Error, gorm.ErrRecordNotFound) {
 		return nil
 	} else if qResult.Error != nil {
